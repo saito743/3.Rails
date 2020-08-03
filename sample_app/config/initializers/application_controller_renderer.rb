4bf1afd0cd316e73ed20ff608ecc8a06a35b8ab0
vagrant <vagrant@localhost.localdomain>
2020-08-03 13:12:08 +0000

    [Update]3-8画像投稿機能追加

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '1e9b5aedbfc4021327b044247d29250aa3a8f6a90cd6269cd3e407d14863300ff84914bdcaa7495baf8b973c11c5aa603f9573f2ccc5a218f02fbb281f2c8a33'
